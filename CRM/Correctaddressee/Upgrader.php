011e62874c4ae2fe5b649839e7b35e4711f32bf9
Jaap Jansma <jaap@edeveloper.nl>
2015-05-12 20:28:07 +0200

    update

--- CRM/Correctaddressee/Upgrader.php
+++ CRM/Correctaddressee/Upgrader.php
@@ -11,7 +11,7 @@ class CRM_Correctaddressee_Upgrader extends CRM_Correctaddressee_Upgrader_Base {
 
   const BATCH_SIZE = 250;
 
-  public function upgrade_1001() {
+  public function upgrade_1002() {
     $maxId = CRM_Core_DAO::singleValueQuery('SELECT max(id) FROM civicrm_contact');
     for ($startId = 0; $startId <= $maxId; $startId += self::BATCH_SIZE) {
       $endId = $startId + self::BATCH_SIZE - 1;
@@ -26,16 +26,47 @@ class CRM_Correctaddressee_Upgrader extends CRM_Correctaddressee_Upgrader_Base {
   }
 
   public static function correct($startId, $endId) {
-    $contact = CRM_Core_DAO::executeQuery("SELECT id, contact_type FROM `civicrm_contact` WHERE `contact_type` = 'Individual' AND `addressee_display` LIKE 'T.a.v.%' AND `id` BETWEEN %1 AND %2", array(1=>array($startId, 'Integer'), 2=>array($endId,'Integer')));
+    $contact = CRM_Core_DAO::executeQuery("SELECT id FROM `civicrm_contact` WHERE `contact_type` = 'Individual' AND `addressee_display` LIKE 'T.a.v.%' AND `id` BETWEEN %1 AND %2", array(1=>array($startId, 'Integer'), 2=>array($endId,'Integer')));
     while ($contact->fetch()) {
-      CRM_Contact_BAO_Contact_Utils::updateGreeting(array(
-        'id' => $contact->id,
-        'gt' => 'addressee',
-        'ct' => $contact->contact_type,
-        'force' => 1,
-      ));
+      self::resetAddressee($contact->id);
     }
     return true;
+  }
+
+  public static function resetAddressee($contact_id) {
+    $filter = array(
+      'contact_type' => 'Individual',
+      'greeting_type' => 'addressee',
+    );
+
+    $allGreetings = CRM_Core_PseudoConstant::greeting($filter);
+    $originalGreetingString = $greetingString = CRM_Utils_Array::value(1, $allGreetings);
+    if (!$greetingString) {
+      CRM_Core_Error::fatal(ts('Incorrect greeting value id %1, or no default greeting for this contact type and greeting type.', array(1 => 1)));
+    }
+
+    // build return properties based on tokens
+    $greetingTokens = CRM_Utils_Token::getTokens($greetingString);
+    $tokens = CRM_Utils_Array::value('contact', $greetingTokens);
+    $greetingsReturnProperties = array();
+    if (is_array($tokens)) {
+      $greetingsReturnProperties = array_fill_keys(array_values($tokens), 1);
+    }
+
+    $extraParams[] = array('contact_type', '=', 'Individual', 0, 0);
+    list($greetingDetails) = CRM_Utils_Token::getTokenDetails(array($contact_id),
+      $greetingsReturnProperties,
+      FALSE, FALSE, $extraParams
+    );
+
+    CRM_Utils_Token::replaceGreetingTokens($greetingString, $greetingDetails[$contact_id], $contact_id, 'CRM_UpdateGreeting');
+
+
+    $sql = "UPDATE `civicrm_contact` SET `addressee_custom` = null, `addressee_id` = 1, `addressee_display` = %1 where id = %2";
+    $sqlParams[1] = array($greetingString, 'String');
+    $sqlParams[2] = array($contact_id, 'Integer');
+
+    CRM_Core_DAO::executeQuery($sql, $sqlParams);
   }
 
 }
